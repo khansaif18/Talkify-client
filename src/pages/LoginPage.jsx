a6fe5cc7af7955941c44fce3d8edb9595654b8c5
SA1FUU <SA1FUU@users.noreply.github.com>
2024-12-08 12:15:52 +0530

    Chotu Changes

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -12,7 +12,7 @@ export default function LoginPage() {
 
     const validateForm = () => {
         if (!formData.email.trim()) return toast.error("Email is required");
-        // if (!/\S+@\S+\.\S+/.test(formData.email)) return toast.error("Invalid email format");
+        if (!formData.password.length < 6) return toast.error('Password must be atleast 6 digits')
         if (!formData.password) return toast.error("Password is required");
         return true
     }
